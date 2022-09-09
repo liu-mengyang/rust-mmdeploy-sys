3c415a69c4efa497ba36681de28ff85bb7e5dc54
Mengyang Liu <myliu@seu.edu.cn>
2022-09-09 14:08:25 +0800

    :bookmark: Release the first version: 0.2.0 - Generate FFI bindings from prebuilt mmDeploy package

--- README.md
+++ README.md
@@ -3,3 +3,33 @@
 MMDeploy bindings for Rust.
 
 :construction: **THIS REPO IS STILL UNDER CONSTRUCTION!**
+
+## Prerequisites
+
+**NOTICE:** Linux only. Onnxruntime only.
+
+In order to successfully build this repo, you are supposed to install some pre-packages.
+
+**Step 1.** Install Clang required by `Bindgen`.
+
+```bash
+apt install llvm-dev libclang-dev clang
+```
+
+**Step 2.** Download and install pre-built mmdeploy package.
+
+```bash
+wget https://github.com/open-mmlab/mmdeploy/releases/download/v0.8.0/mmdeploy-0.8.0-linux-x86_64-onnxruntime1.8.1.tar.gz
+tar -zxvf mmdeploy-0.8.0-linux-x86_64-onnxruntime1.8.1.tar.gz
+cd mmdeploy-0.8.0-linux-x86_64-onnxruntime1.8.1.tar.gz
+export $MMDEPLOY_DIR=$(pwd)
+```
+
+## Build
+
+Move to the root of this repo and then:
+
+```bash
+cargo build
+```
+
--- build.rs
+++ build.rs
@@ -1,8 +1,26 @@
 extern crate bindgen;
 
 use std::io;
+use std::env;
+use std::path::PathBuf;
+use std::fs;
 
 
+struct Library {
+    name: &'static str,
+}
+
+
+static LIBRARIES: &[Library] = &[
+    Library {
+        name: "mmdeploy"
+    }
+];
+
+
+fn output() -> PathBuf {
+    PathBuf::from(env::var("OUT_DIR").unwrap())
+}
 
 
 fn build() -> io::Result<()> {
@@ -11,6 +29,78 @@ fn build() -> io::Result<()> {
     Ok(())
 }
 
-fn main() {
 
+fn link_to_libraries() {
+    // let mmdeploy_ty = if statik { "static" } else { "dylib" };
+    let mmdeploy_ty = "dylib";
+    for lib in LIBRARIES {
+        println!("cargo:rustc-link-lib={}={}", mmdeploy_ty, lib.name);
+    }
+}
+
+
+fn search_include(include_paths: &[PathBuf], header: &str) -> String {
+    for dir in include_paths {
+        let include = dir.join(header);
+        if fs::metadata(&include).is_ok() {
+            return include.as_path().to_str().unwrap().to_string();
+        }
+    }
+    format!("/usr/include/{}", header)
+}
+
+
+fn main() {
+    // let statik = env::var("CARGO_FEATURE_STATIC").is_ok();
+    let include_path: Vec<PathBuf> = if env::var("CARGO_FEATURE_BUILD").is_ok() {
+        // TODO: Support static build mode.
+        println!("Static build mode");
+
+        vec![]
+    } else if let Ok(mmdeploy_dir) = env::var("MMDEPLOY_DIR") {
+        let mmdeploy_dir = PathBuf::from(mmdeploy_dir);
+        println!(
+            "cargo:rustc-link-search=native={}",
+            mmdeploy_dir.join("sdk").join("lib").to_string_lossy()
+        );
+        link_to_libraries();
+        vec![mmdeploy_dir.join("sdk").join("include").join("mmdeploy")]
+    } else {
+        // TODO: Support pkg-config
+        println!("Fallback to pkg-config");
+
+        vec![]
+    };
+
+    let clang_includes = include_path
+        .iter()
+        .map(|include| format!("-I{}", include.to_string_lossy()));
+
+    // create builder
+    let mut builder = bindgen::Builder::default()
+        .clang_args(clang_includes);
+
+    builder = builder
+        .header(search_include(&include_path, "common.h"))
+        .header(search_include(&include_path, "model.h"))
+        .header(search_include(&include_path, "executor.h"))
+        .header(search_include(&include_path, "pipeline.h"))
+        .header(search_include(&include_path, "classifier.h"))
+        .header(search_include(&include_path, "detector.h"))
+        .header(search_include(&include_path, "segmentor.h"))
+        .header(search_include(&include_path, "pose_detector.h"))
+        .header(search_include(&include_path, "rotated_detector.h"))
+        .header(search_include(&include_path, "text_recognizer.h"))
+        .header(search_include(&include_path, "text_detector.h"))
+        .header(search_include(&include_path, "restorer.h"));
+
+    // generate builder
+    let bindings = builder
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    bindings
+        .write_to_file(output().join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
