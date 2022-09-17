d06de05e1fb89b35c2e6240bba57b80614a1e7c9
Mengyang Liu <myliu@seu.edu.cn>
2022-09-17 19:53:06 +0800

    :bookmark: Release the second version: 0.3.0 - Fix typo and add wrapper for detector

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+impl mmdeploy_detector {
+    pub fn new() -> mmdeploy_detector {
+        mmdeploy_detector {
+            _unused: [1; 0],
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
